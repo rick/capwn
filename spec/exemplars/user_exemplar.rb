f2eca4273df1da35cfede58b1d47b603b0ae71cf
Arafat Mohamed <amohamed@gmail.com>
2008-07-31 18:52:36 -0400

    Formatted code in user examplar

--- spec/exemplars/user_exemplar.rb
+++ spec/exemplars/user_exemplar.rb
@@ -2,23 +2,24 @@ class User
 
   generator_for :login, :start => 'Login000' do |prev|
     prev.succ
+  end
 
-end
-
-generator_for :name, :start => 'Name 000' do |prev|
-  name, id = prev.split(/ /)
-  "#{name} #{id.succ}"
-end
-generator_for :email, :start => 'email000@domain.com' do |prev|
-  user, domain = prev.split(/@/)
-  "#{user.succ}@#{domain}"
-end
-generator_for :password, :start => 'password000' do |prev|
-  prev.succ
-end
-generator_for :password_confirmation, :start => 'password000' do |prev|
-  prev.succ
-end
-generator_for :created_at, 5.days.ago 
-generator_for :salt,       '7e3041ebc2fc05a40c60028e2c4901a81035d3cd'
+  generator_for :name, :start => 'Name 000' do |prev|
+    name, id = prev.split(/ /)
+    "#{name} #{id.succ}"
+  end
+  generator_for :email, :start => 'email000@domain.com' do |prev|
+    user, domain = prev.split(/@/)
+    "#{user.succ}@#{domain}"
+  end
+  generator_for :password, :start => 'password000' do |prev|
+    prev.succ
+  end
+  generator_for :password_confirmation, :start => 'password000' do |prev|
+    prev.succ
+  end
+  generator_for :created_at do 
+    5.days.ago 
+  end
+  generator_for :salt,       '7e3041ebc2fc05a40c60028e2c4901a81035d3cd'
 end
