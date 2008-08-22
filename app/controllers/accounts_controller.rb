f8b45ed3ad1e8c0fe40024eb4206977da8930311
Arafat Mohamed <github@arafatm.com>
2008-08-21 22:15:55 -0500

    Redirecting to index on successful account creation instead of to new

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,15 @@
 class AccountsController < ApplicationController
   resources_controller_for :accounts
 
+  def create
+    self.resource = new_resource
+    if resource.save
+      redirect_to accounts_path
+    else
+      render :action => "new"
+    end
+  end
+
   def find_resources
     resource_service.find :all, :order => 'name'
   end
