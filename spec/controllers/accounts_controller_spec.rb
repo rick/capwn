d7b32fb81673af9a0da1ee47a3cfa2eb6540321c
Arafat Mohamed <github@arafatm.com>
2008-08-21 21:54:15 -0500

    Using redirect_to_index to notify shared spec that create redirects to index

--- spec/controllers/accounts_controller_spec.rb
+++ spec/controllers/accounts_controller_spec.rb
@@ -1,6 +1,9 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe AccountsController do
+  def redirects_to_index 
+    true
+  end
   it_should_behave_like 'a RESTful controller requiring login'
 end
 
