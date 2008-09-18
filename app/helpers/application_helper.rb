8941470fff8883b047447857a3a05ac0d37a5819
Arafat Mohamed <github@arafatm.com>
2008-09-18 18:41:36 -0500

    Added helper currencize to format currencies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def currencize(initialBalance)
+    n = sprintf("%.2f", initialBalance)
+    n.to_s =~ /([^\.]*)(\..*)?/
+    int, dec = $1.reverse, $2 ? $2 : ""
+    while int.gsub!(/(,|\.|^)(\d{3})(\d)/, '\1\2,\3')
+    end
+    int.reverse + dec
+  end
 end
--- app/models/account.rb
+++ app/models/account.rb
@@ -10,13 +10,4 @@ class Account < ActiveRecord::Base
   validates_presence_of :initialBalance
   validates_numericality_of :initialBalance, :greater_than_or_equal_to => 0.00
 
-  def formatted_initial_balance
-    n = sprintf("%.2f", self.initialBalance)
-    n.to_s =~ /([^\.]*)(\..*)?/
-    int, dec = $1.reverse, $2 ? $2 : ""
-    while int.gsub!(/(,|\.|^)(\d{3})(\d)/, '\1\2,\3')
-    end
-    int.reverse + dec
-
-  end
 end
