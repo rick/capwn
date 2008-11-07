ba3b51fb742e4f608976bf06fcded36055405d27
Arafat Mohamed <github@arafatm.com>
2008-11-06 22:59:36 -0500

    Fixed references to initialBalance to use changed initial_balance

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def currencize(initialBalance)
-    n = sprintf("%.2f", initialBalance)
+  def currencize(initial_balance)
+    n = sprintf("%.2f", initial_balance)
     n.to_s =~ /([^\.]*)(\..*)?/
     int, dec = $1.reverse, $2 ? $2 : ""
     while int.gsub!(/(,|\.|^)(\d{3})(\d)/, '\1\2,\3')
