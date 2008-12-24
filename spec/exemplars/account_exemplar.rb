c1534fdc946bdce416ed5c22f765bebf8e3658c6
Arafat Mohamed <github@arafatm.com>
2008-12-24 01:58:15 -0500

    Create random starting balance on Account exemplar

--- spec/exemplars/account_exemplar.rb
+++ spec/exemplars/account_exemplar.rb
@@ -2,7 +2,11 @@ class Account
   generator_for :name, :start => 'Test Account 1' do |prev|
     prev.succ
   end
-  generator_for :initial_balance, rand(99999)
+  generator_for :initial_balance, :method => :random_balance
   generator_for :active, true
   generator_for :element, 'Asset'
+
+  def self.random_balance
+    rand(9999)
+  end
 end
