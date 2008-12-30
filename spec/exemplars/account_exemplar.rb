ae0e45fb0b713ce18e332f344325a5ff869ed96c
Arafat Mohamed <github@arafatm.com>
2008-12-30 17:03:09 -0500

    Updated account exemplar to generate random element and active

--- spec/exemplars/account_exemplar.rb
+++ spec/exemplars/account_exemplar.rb
@@ -3,10 +3,19 @@ class Account
     prev.succ
   end
   generator_for :initial_balance, :method => :random_balance
-  generator_for :active, true
-  generator_for :element, 'Asset'
+  generator_for :active, :method => :random_true
+  generator_for :element, :method => :random_element
 
   def self.random_balance
     rand(9999)
+  end
+
+  def self.random_true
+    rand(2) == 1 ? true : false
+  end
+
+  def self.random_element
+    elements = ['Asset', 'Liability', 'Equity', 'Revenue', 'Expense']
+    elements[rand(elements.size)]
   end
 end
