53540c7c01ee2518ef38ed76238b6494df827b90
Arafat Mohamed <github@arafatm.com>
2008-12-30 18:02:13 -0500

    Account generator sets active = true by default. Changing this broke a bunch of specs

--- spec/exemplars/account_exemplar.rb
+++ spec/exemplars/account_exemplar.rb
@@ -1,17 +1,13 @@
 class Account
-  generator_for :name, :start => 'Test Account 1' do |prev|
+  generator_for :name, :start => 'Test Account 000' do |prev|
     prev.succ
   end
   generator_for :initial_balance, :method => :random_balance
-  generator_for :active, :method => :random_true
+  generator_for :active, true
   generator_for :element, :method => :random_element
 
   def self.random_balance
     rand(9999)
-  end
-
-  def self.random_true
-    rand(2) == 1 ? true : false
   end
 
   def self.random_element
